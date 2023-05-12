16e01592ec1921f037651fbc2bd8db4a9f02b0fa
Cody <59001842+hard-shutdown@users.noreply.github.com>
2023-05-12 01:02:55 +0000

    add more stuff looks better is better

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,28 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-48">
-      <h1 className="text-4xl font-bold text-center">
-        Tweeter 
-      </h1>
-      <form className="flex flex-col items-center justify-center">
-        <input id="authorname" type="text" placeholder="Author Name" className="border-2 border-gray-300 rounded-md p-2 m-2" />
-        <textarea id="tweet" placeholder="Tweet" className="border-2 border-gray-300 rounded-md p-2 m-2" />
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Tweet
-        </button>
-      </form>
-    </main>
+    <>
+      <div className="absolute w-screen h-screen bg-gray-100 opacity-50 top-0 left-0" style={{
+        zIndex: -1
+      }}></div>
+      <main className="flex min-h-screen flex-col justify-between p-48">
+        <div className="absolute top-10 left-0 w-screen">
+          <h1 className="text-4xl font-bold text-center">
+            Tweeter
+          </h1>
+        </div>
+        <div>
+          <div className="w-96 h-48 mt-10 float-left">
+            <form className="flex flex-col space-y-3 m-auto">
+              <input className="border border-gray-400 rounded-md p-2" type="text" placeholder="Username" />
+              <textarea className="border border-gray-400 rounded-md p-2" placeholder="What's happening?" />
+              <button className="bg-blue-500 text-white rounded-md p-2">Tweet</button>
+            </form>
+          </div>
+          <div className="w-96 h-1/2 bg-green-500 float-right">
+            <h1>Feed</h1>
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
